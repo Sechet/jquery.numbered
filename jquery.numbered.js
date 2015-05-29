ad49971ace359fe47ec72a692153f191be79693e
pavel-yagodin <pavel.yagodin@gmail.com>
2015-05-29 21:46:07 +0300

    v0.1.7 - feat paste

--- jquery.numbered.js
+++ jquery.numbered.js
@@ -17,10 +17,6 @@
 			attrPlaceholder: 'data-placeholder',
 			placeholder: false
 		}, options);
-
-		if($(this).hasClass(options.inputClass)){
-			return $(this);
-		}
 		var pluginName = 'numbered',
 			pluginValue = 'numberedValue',
 			dataPlugin = 'plugin_' + pluginName;
@@ -150,7 +146,6 @@
 			return _this;
 		};
 		$(this).each(function(){
-
 			var data = {};
 			data.$input = $(this);
 			data.paste = false;
@@ -158,11 +153,15 @@
 			data.pasteOld = '';
 			data.posFocus = 0;
 			data.keyState = false;
-			if(data.$input.attr('placeholder') !== undefined && options.placeholder === false){
-				options.placeholder = data.$input.attr('placeholder');
-				data.$input.attr('data-placeholder', options.placeholder);
-				data.$input.attr('placeholder', '');
+			data.placeholder = options.placeholder;
+			if(data.$input.attr('placeholder') !== undefined && data.$input.attr('placeholder') !== '' && data.placeholder === false){
+				data.placeholder = data.$input.attr('placeholder');
 			}
+			if(data.placeholder !== false){
+				data.$input.attr('data-placeholder', data.placeholder);
+			}
+			data.$input.attr('placeholder', '');
+
 			if( typeof options.mask === 'string'){
 				options.max = (options.mask).split(options.maskKey).length - 1;
 				options.mask = (options.mask).split('');
@@ -171,184 +170,207 @@
 			data.value = data.$input.val().replace(/\D/, '');
 			data.value = (data.value.substr(0, options.max)).split('');
 
-
-			data.$input
-				.data(pluginValue, data.value)
-				.attr('autocomplete', 'off')
-				.addClass(options.inputClass)
-				.wrap('<div class="'+options.wrapClass+'"></div>');
-			data.$wrap = data.$input.parent();
-			data.$wrap.append('<span class="'+options.spanClass+'"></span>');
-			data.$span = data.$wrap.find('span');
-			data.$wrap
-				.on('click', function(){
-					data.$input.focus();
-				});
-			if (/Android/i.test(navigator.userAgent)) {
-				data.$input.attr('type', 'tel')
-				if(/Android 4.0/i.test(navigator.userAgent)){
-					data.options.empty = ' '
-				}
-			}
-
-			if (/Android/i.test(navigator.userAgent) && /YaBrowser/i.test(navigator.userAgent) ) {
-				var text = '';
-				for (var key in data.options.mask) {
-					var val = data.options.mask[key];
-					if(val === data.options.maskKey){
-						text += (data.options.empty!==false)?data.options.empty:'_';
-					}else{
-						text += val;
-					}
-				}
-				data.$span.html(text);
-				data.$input.on('focus.numbered input.numbered', function(e) {
-					data.$input.val(data.$input.val());
-					data.$wrap.addClass(data.options.wrapFocusClass);
-				});
-				data.$input.on('focusout.numbered', function(e) {
-					if(this.value === ''){
-						data.$wrap.removeClass(data.options.wrapFocusClass);
-					}else{
-						data.$wrap.addClass(data.options.wrapFocusClass);
-					}
-				});
-				data.$input.trigger('focusout');
-
-			} else {
-				data.$wrap.attr(options.attrPlaceholder, (options.placeholder===false)?'':options.placeholder);
-				$(window).on('beforeunload.numbered', function(){
-					data.$input.trigger('focusout');
-				});
+			if(data.$input.data('numbered')==='init'){
 				data.$input
-					.on('change.numbered', function() {
-						data.value = this.value.replace(/\D/, '');
-						data.value = (data.value.substr(0, options.max)).split('');
-						data.$input
-							.data(pluginValue, data.value);
-						data.posFocus = getPos(data, data.value.length);
-						data = make(data, 'change');
-					})
-					.on('focusout.numbered', function() {
-						data = make(data, 'focusout');
-					})
-					.on('paste.numbered', function() {
-						data.paste = true;
-						data.pasteOld = data.$input.val();
-					})
-					.on('keyup.numbered click.numbered focusin.numbered input.numbered', function(e) {
-						if($(this).attr('readonly') === 'readonly'){
-							return false;
+					.data(pluginValue, data.value)
+			}else{
+				data.$input
+					.data(pluginValue, data.value)
+					.data('numbered', 'init')
+					.attr('autocomplete', 'off')
+					.addClass(options.inputClass)
+					.wrap('<div class="'+options.wrapClass+'"></div>');
+
+				data.$wrap = data.$input.parent();
+				data.$wrap.append('<span class="'+options.spanClass+'"></span>');
+				data.$span = data.$wrap.find('span');
+				data.$wrap
+					.on('click', function(){
+						data.$input.focus();
+					});
+				if (/Android/i.test(navigator.userAgent)) {
+					data.$input.attr('type', 'tel')
+					if(/Android 4.0/i.test(navigator.userAgent)){
+						data.options.empty = ' '
+					}
+				}
+
+				if (/Android/i.test(navigator.userAgent) && /YaBrowser/i.test(navigator.userAgent) ) {
+					var text = '';
+					for (var key in data.options.mask) {
+						var val = data.options.mask[key];
+						if(val === data.options.maskKey){
+							text += (data.options.empty!==false)?data.options.empty:'_';
+						}else{
+							text += val;
 						}
-						if(!data.options.chars){
-							if (data.paste) {
-								if (e.type == 'input') {
-									var value = data.$input.data(pluginValue),
-										paste = data.$input.val();
-									if (data.posFocus !== 0) {
-										paste = paste.substr(data.posFocus);
-									} else {
-										paste = paste.replace(data.pasteOld, '');
-									}
-									for (var key in paste) {
-										var val = paste[key];
-										if(value.length < data.options.max && parseInt(val) >= 0){
-											value.push(val);
+					}
+					data.$span.html(text);
+					data.$input.on('focus.numbered input.numbered', function(e) {
+						data.$input.val(data.$input.val());
+						data.$wrap.addClass(data.options.wrapFocusClass);
+					});
+					data.$input.on('focusout.numbered', function(e) {
+						if(this.value === ''){
+							data.$wrap.removeClass(data.options.wrapFocusClass);
+						}else{
+							data.$wrap.addClass(data.options.wrapFocusClass);
+						}
+					});
+					data.$input.trigger('focusout');
+
+				} else {
+					data.$wrap.attr(options.attrPlaceholder, (data.placeholder===false)?'':data.placeholder);
+					$(window).on('beforeunload.numbered', function(){
+						data.$input.trigger('focusout');
+					});
+					data.$input
+						.on('change.numbered', function() {
+							data.value = this.value.replace(/\D/, '');
+							data.value = (data.value.substr(0, options.max)).split('');
+							data.$input
+								.data(pluginValue, data.value);
+							data.posFocus = getPos(data, data.value.length);
+							data = make(data, 'change');
+						})
+						.on('focusout.numbered', function() {
+							data = make(data, 'focusout');
+						})
+						.on('paste.numbered', function() {
+							data.paste = true;
+							data.pasteOld = data.$input.val();
+							setTimeout(function(){
+								var val = data.$input.data(pluginValue)
+								data.posFocus = getPos(data, val.length);
+								data = make(data, 'input');
+								console.log(': '+data.value)
+							}, 10);
+							console.log('test')
+
+						})
+						.on('keyup.numbered click.numbered focusin.numbered input.numbered', function(e) {
+
+							console.log(e.type+': '+data.value)
+							if($(this).attr('readonly') === 'readonly'){
+								return false;
+							}
+							if(!data.options.chars){
+								if (data.paste) {
+									if (e.type == 'input') {
+										var value = data.$input.data(pluginValue),
+											paste = data.$input.val();
+										if (data.posFocus !== 0) {
+											paste = paste.substr(data.posFocus);
+										} else {
+											paste = paste.replace(data.pasteOld, '');
 										}
+										for (var key in paste) {
+											var val = paste[key];
+											if(value.length < data.options.max && parseInt(val) >= 0){
+												value.push(val);
+											}
+										}
+										data.$input.data(pluginValue, value);
+										data.paste = false;
 									}
-									data.$input.data(pluginValue, value);
-									data.paste = false;
 								}
+
+								if(e.type === 'click' && data.focus){
+									data = make(data, 'caret');
+								} else {
+									data = make(data, 'input', data.posFocus);
+								}
+							}
+							if(e.type === 'focusin'){
+								data.focus = true;
+							}
+							if(e.type === 'keyup'){
+								data.focus = true;
 							}
 
-							if(e.type === 'click' && data.focus){
-								data = make(data, 'caret');
-							} else {
-								data = make(data, 'input', data.posFocus);
-							}
-						}
-						if(e.type === 'focusin'){
-							data.focus = true;
-						}
-					})
-					.on('input.numbered', function(e) {
-						return false;
-					})
-					.on('keydown.numbered', function(e) {
-						if($(this).attr('readonly') === 'readonly'){
+						})
+						.on('input.numbered', function(e) {
 							return false;
-						}
-						if (e.keyCode !== 9) {
-							e.preventDefault();
-						}else{
-							return true;
-						}
-						if(!data.options.chars){
-							data.options.chars = true;
-							var value = data.$input.data(pluginValue),
-								newChar = '';
-							if (e.keyCode <= 105 && e.keyCode >= 96) {
-								newChar = (e.keyCode-96)+'';
+						})
+						.on('keydown.numbered', function(e) {
+							if(e.ctrlKey && e.keyCode === 86){
+								return true;
 							}
-							if (e.keyCode <= 57 && e.keyCode >= 48 && !e.shiftKey) {
-								newChar = (e.keyCode-48)+'';
+							if($(this).attr('readonly') === 'readonly'){
+								return false;
 							}
-							if(/^\d$/.test(String.fromCharCode(e.which)) && !e.shiftKey){
-								newChar = String.fromCharCode(e.which);
+							if (e.keyCode !== 9) {
+								e.preventDefault();
+							}else{
+								return true;
 							}
-							if (e.keyCode == 8) {
-								pos = getIndex(data, data.posFocus);
-								if ( pos > 0 ) {
-									value.splice(pos-1, 1);
-									data.posFocus = getPos(data, pos-1);
+							if(!data.options.chars){
+								data.options.chars = true;
+								var value = data.$input.data(pluginValue),
+									newChar = '';
+								if (e.keyCode <= 105 && e.keyCode >= 96) {
+									newChar = (e.keyCode-96)+'';
 								}
-							}
-							if (e.keyCode == 46) {
-								pos = getIndex(data, data.posFocus);
-								value.splice(pos, 1);
-							}
-							if(e.keyCode == 36 || e.keyCode == 38){
-								data.posFocus = getPos(data, 0);
-							}
-							if(e.keyCode == 35 || e.keyCode == 40){
-								data.posFocus = getPos(data, value.length);
-								if( value.length === data.options.max ){
-									data.posFocus++;
+								if (e.keyCode <= 57 && e.keyCode >= 48 && !e.shiftKey) {
+									newChar = (e.keyCode-48)+'';
 								}
-							}
-							if(e.keyCode == 37){
-								pos = getIndex(data, data.posFocus);
-								if (pos > 0) {
-									pos--;
+								if(/^\d$/.test(String.fromCharCode(e.which)) && !e.shiftKey){
+									newChar = String.fromCharCode(e.which);
 								}
-								data.posFocus = getPos(data, pos);
-							}
-							if(e.keyCode == 39){
-								pos = getIndex(data, data.posFocus);
-								if( pos < value.length ){
-									pos++;
-								}
-								data.posFocus = getPos(data, pos);
-							}
-							if (newChar !== '') {
-								if(value.length < data.options.max){
+								if (e.keyCode == 8) {
 									pos = getIndex(data, data.posFocus);
-									value.splice(pos, 0, newChar);
-									data.posFocus = getPos(data, pos+1);
-									if(value.length == data.options.max){
+									if ( pos > 0 ) {
+										value.splice(pos-1, 1);
+										data.posFocus = getPos(data, pos-1);
+									}
+								}
+								if (e.keyCode == 46) {
+									pos = getIndex(data, data.posFocus);
+									value.splice(pos, 1);
+								}
+								if(e.keyCode == 36 || e.keyCode == 38){
+									data.posFocus = getPos(data, 0);
+								}
+								if(e.keyCode == 35 || e.keyCode == 40){
+									data.posFocus = getPos(data, value.length);
+									if( value.length === data.options.max ){
 										data.posFocus++;
 									}
 								}
+								if(e.keyCode == 37){
+									pos = getIndex(data, data.posFocus);
+									if (pos > 0) {
+										pos--;
+									}
+									data.posFocus = getPos(data, pos);
+								}
+								if(e.keyCode == 39){
+									pos = getIndex(data, data.posFocus);
+									if( pos < value.length ){
+										pos++;
+									}
+									data.posFocus = getPos(data, pos);
+								}
+								if (newChar !== '') {
+									if(value.length < data.options.max){
+										pos = getIndex(data, data.posFocus);
+										value.splice(pos, 0, newChar);
+										data.posFocus = getPos(data, pos+1);
+										if(value.length == data.options.max){
+											data.posFocus++;
+										}
+									}
+								}
+								data.$input.data(pluginValue, value);
+								data = make(data, 'input', data.posFocus);
+							}else{
+								e.preventDefault();
 							}
-							data.$input.data(pluginValue, value);
-							data = make(data, 'input', data.posFocus);
-						}else{
-							e.preventDefault();
-						}
-						return false;
-					});
-				data.posFocus = getPos(data, 0);
-				data = make(data, 'focusout');
+							return false;
+						});
+					data.posFocus = getPos(data, 0);
+					data = make(data, 'focusout');
+				}
 			}
 		});
 		return this;
